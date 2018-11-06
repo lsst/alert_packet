9ba147ace0e1491f268538045e5147973a2b3939
Eric Bellm <ecbellm@uw.edu>
2018-11-06 00:16:01 -0500

    generate functional random packets

--- generateFakePackets.py
+++ generateFakePackets.py
@@ -1,15 +1,10 @@
 
 
+import string
+import random
 import numpy as np
+import fastavro
 from validateAvroNestedSchema import load_stamp, combine_schemas
-
-cutout_path = './examples/stamp-676.fits.gz'
-
-schemas = ['schema/cutout.avsc','schema/diaforcedsource.avsc',
-           'schema/dianondetectionlimit.avsc','schema/diasource.avsc',
-           'schema/diaobject.avsc','schema/ssobject.avsc','schema/alert.avsc']
-
-schema = combine_schemas(schemas)
 
 
 def randomNull():
@@ -17,62 +12,80 @@ def randomNull():
     """
     return None
 
+
 def randomBoolean():
     """Provide a random value of the Avro `boolean` type.
     """
     return bool(np.random.randint(2))
 
+
 def randomInt():
     """Provide a random value of the Avro `int` type.
     """
-    return np.random.randint(np.iinfo(np.int32).min, 
-                             high = np.iinfo(np.int32).max, dtype=np.int32)
+    return int(np.random.randint(np.iinfo(np.int32).min,
+                                 high=np.iinfo(np.int32).max, dtype=np.int32))
+
 
 def randomLong():
     """Provide a random value of the Avro `long` type.
     """
-    return np.random.randint(np.iinfo(np.int64).min, 
-                             high = np.iinfo(np.int64).max, dtype=np.int64)
+    return int(np.random.randint(np.iinfo(np.int64).min,
+                                 high=np.iinfo(np.int64).max, dtype=np.int64))
+
 
 def randomFloat():
     """Provide a random value of the Avro `float` type.
     """
-    return (np.random.rand() * np.finfo(np.float32).max).astype(np.float32)
+    return float((np.random.rand() * np.finfo(np.float32).max).astype(np.float32))
+
 
 def randomDouble():
     """Provide a random value of the Avro `double` type.
     """
-    return (np.random.rand() * np.finfo(np.float64).max).astype(np.float64)
+    return float((np.random.rand() * np.finfo(np.float64).max).astype(np.float64))
+
 
 def randomString():
     """Provide a random value of the Avro `string` type.
     """
-    return np.random.bytes(np.random.randint(0,100)).decode(errors='replace')
+    def rand_str(n): return ''.join([random.choice(string.ascii_lowercase) for i in range(n)])
+    return rand_str(np.random.randint(0, 100))
+
 
 def randomRecord():
     """Provide a random value of the Avro `record` type.
     """
     return build_random_record
 
+
 def randomArray():
     """Provide a random value of the Avro `array` type.
     """
     return build_random_array
 
+
+def randomBytes():
+    """Provide a random value of the Avro `bytes` type.
+    """
+    cutout_path = './examples/stamp-676.fits.gz'
+    return load_stamp(cutout_path)['stampData']
+
+
 randomizerFunctionsByType = {
-        'null': randomNull,
-        'boolean': randomBoolean,
-        'int': randomInt,
-        'long': randomLong,
-        'float': randomFloat,
-        'double': randomDouble,
-        'string': randomString
-        # exclude "bytes" dtype because we will handle cutouts specially
+    'null': randomNull,
+    'boolean': randomBoolean,
+    'int': randomInt,
+    'long': randomLong,
+    'float': randomFloat,
+    'double': randomDouble,
+    'string': randomString,
+    'bytes': randomBytes
 }
 
 
-
-def walk_schema(schema, keepNull = None, arrayCount = None):
+def createFakeJsonPacket(schema, keepNull=None, arrayCount=None):
+    """Parse JSON schema and generate random values.
+    """
 
     output = {}
 
@@ -85,10 +98,10 @@ def walk_schema(schema, keepNull = None, arrayCount = None):
     if type(schema['type']) == list:
         # potentially nullable
         if ('null' in schema['type']) and (schema['name'] in keepNull):
-            print(schema['name'], schema['type'])
             return {schema['name']: None}
         else:
-            # this is not general, but our schema is only fixed types and nulls
+            # inferring the type like this this is not general but works
+            # for our application
             schema['type'] = schema['type'][0]
 
     if type(schema['type']) == dict:
@@ -97,28 +110,57 @@ def walk_schema(schema, keepNull = None, arrayCount = None):
             if schema['name'] in arrayCount:
                 output_array = []
                 for i in range(arrayCount[schema['name']]):
-                    output_array.append(walk_schema(schema['type']['items']))
+                    output_array.append(createFakeJsonPacket(
+                        schema['type']['items'], keepNull=keepNull, arrayCount=arrayCount))
                 return {schema['name']: output_array}
             else:
                 return {schema['name']: None}
         elif schema['type'] == 'record':
             for field in schema['fields']:
-                output.update(walk_schema(field))
+                output.update(createFakeJsonPacket(field, keepNull=keepNull, arrayCount=arrayCount))
                 return output
         else:
-            # a nested type 
-            nest = {schema['name']:walk_schema(schema['type'])}
+            # a nested type
+            nest = {schema['name']: createFakeJsonPacket(
+                schema['type'], keepNull=keepNull, arrayCount=arrayCount)}
             output.update(nest)
             return output
 
     if schema['type'] == 'record':
         for field in schema['fields']:
-            output.update(walk_schema(field))
-    elif schema['type'] in ['null', 'boolean', 'int', 'long', 'float', 'double', 'string']:
-        return {schema['name'] : randomizerFunctionsByType[schema['type']]()}
-    elif schema['type'] == 'bytes':
-        return {schema['name'] : load_stamp(cutout_path) }
+            output.update(createFakeJsonPacket(field, keepNull=keepNull, arrayCount=arrayCount))
+    elif schema['type'] in randomizerFunctionsByType.keys():
+        return {schema['name']: randomizerFunctionsByType[schema['type']]()}
     else:
         raise ValueError('Parsing broke...')
 
     return output
+
+
+def createFakePacket(writeSchema=False):
+    """Serialize a fake packet to disk.
+    """
+
+    outfile = 'test.avro'
+
+    schemas = ['schema/cutout.avsc', 'schema/diaforcedsource.avsc',
+               'schema/dianondetectionlimit.avsc', 'schema/diasource.avsc',
+               'schema/diaobject.avsc', 'schema/ssobject.avsc', 'schema/alert.avsc']
+
+    jsonSchema = combine_schemas(schemas)
+
+    # values from LSE-81
+    avgYearlyVisits = int(1056/10)
+
+    keepNull = ['ssObject']
+    arrayCount = {'prvDiaSources': avgYearlyVisits,
+                  'prvDiaForcedSources': int(avgYearlyVisits/12),
+                  'prvDiaNondetectionLimits': 0}
+
+    jsonData = createFakeJsonPacket(jsonSchema, keepNull=keepNull, arrayCount=arrayCount)
+
+    with open(outfile, 'wb') as f:
+        if writeSchema:
+            fastavro.writer(f, jsonSchema, jsonData)
+        else:
+            fastavro.schemaless_writer(f, jsonSchema, jsonData)
