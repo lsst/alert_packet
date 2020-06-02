d211f609459633feefa8d6e0551136df76f2247b
Spencer Nelson <swnelson@uw.edu>
2020-06-02 14:29:17 -0700

    Replace 'latest' symlink in schema with a file
    This is unfortunate, but symlinks are not supported within setuptools
    Python packages, so we really have no option other than to get rid of
    the symlinks which point to the latest version of our schemas.
    Instead, we need to use a text file.

--- bin/validateAvroRoundTrip.py
+++ bin/validateAvroRoundTrip.py
@@ -75,11 +75,10 @@ def parse_args():
 
 def main(args):
     if args.schema_version == "latest":
-        schema_root = os.path.join(lsst.alert.packet.get_schema_root(), "latest")
+        schema_major, schema_minor = lsst.alert.packet.get_latest_schema_version()
     else:
         schema_major, schema_minor = args.schema_version.split(".")
-        schema_root = os.path.join(lsst.alert.packet.get_schema_root(),
-                                   f"{schema_major}/{schema_minor}")
+    schema_root = lsst.alert.packet.get_schema_path(schema_major, schema_minor)
 
     alert_schema = lsst.alert.packet.Schema.from_file(os.path.join(schema_root,
                                                             SCHEMA_FILENAME))
