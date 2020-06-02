8ee6483cf622ba8d51ae5b567dbd86915ab45daf
Spencer Nelson <swnelson@uw.edu>
2020-06-02 14:30:27 -0700

    Add setup.py
    This is a first crack at a setup.py script for the package. It uses
    package data to ensure that binary distributions include the schema
    files.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+import os
+
+package_files = []
+for dir, subdirs, files in os.walk("schema"):
+    for d in subdirs:
+        package_files.append(dir + "/" + d)
+    for f in files:
+        package_files.append(dir + "/" + f)
+
+setup(name='lsst-alert-packet',
+      version="0.1.0",
+      description='Code for interacting with Vera Rubin Observatory alert packets',
+      url='https://github.com/lsst/alert_packet',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Development Status :: 3 - Alpha',
+      ],
+      author='Spencer Nelson',
+      author_email='swnelson@uw.edu',
+      license='GPLv3',
+      packages=['lsst.alert.packet'],
+      package_dir={'': 'python'},
+      package_data={'lsst.alert.packet': package_files},
+      install_requires=['fastavro', 'numpy'],
+      scripts=[
+          'bin/simulateAlerts.py',
+          'bin/validateAvroRoundTrip.py',
+      ],
+      zip_safe=False)
