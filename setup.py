1b1d0691233b05b745771f385b2a9897def188ae
Spencer Nelson <swnelson@uw.edu>
2020-06-04 09:33:17 -0700

    Move all setuptools configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
 from setuptools import setup
 
-import os
-import posixpath
-
-package_files = []
-for dir, subdirs, files in os.walk("schema"):
-    # Note that posixpath is right here and os.path is wrong. setuptools expects
-    # slash-delimited paths even on windows.
-    for d in subdirs:
-        package_files.append(posixpath.join(dir, d))
-    for f in files:
-        package_files.append(posixpath.join(dir, f))
-
-
-setup(name='lsst-alert-packet',
-      version="0.1.0",
-      description='Code for interacting with Vera C. Rubin Observatory alert packets',
-      url='https://github.com/lsst/alert_packet',
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Development Status :: 3 - Alpha',
-      ],
-      author='Spencer Nelson',
-      author_email='swnelson@uw.edu',
-      license='GPLv3',
-      packages=['lsst.alert.packet'],
-      package_dir={'': 'python'},
-      package_data={'lsst.alert.packet': package_files},
-      install_requires=['fastavro', 'numpy'],
-      scripts=[
-          'bin/simulateAlerts.py',
-          'bin/validateAvroRoundTrip.py',
-      ])
+# All configuration is in setup.cfg.
+setup()
--- test/test_setup.py.sh
+++ test/test_setup.py.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cd ../
+REPO_ROOT=$(git rev-parse --show-toplevel)
 
 TEMP_VENV_DIR=$(mktemp -d)
 virtualenv $TEMP_VENV_DIR
 source $TEMP_VENV_DIR/bin/activate
 
 TEMP_DIST_DIR=$(mktemp -d)
-python ../setup.py bdist_wheel --dist-dir=$TEMP_DIST_DIR
+python $REPO_ROOT/setup.py bdist_wheel --dist-dir=$TEMP_DIST_DIR
 
 pip install $TEMP_DIST_DIR/*
 validateAvroRoundTrip.py
