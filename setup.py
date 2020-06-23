6d2c7a5992eeb42a11483ac72d8c8678ab782f05
John Swinbank <swinbank@lsst.org>
2020-06-23 09:05:44 -0700

    Write version.py when building with setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
 
+# version.py is created when building with SCons.
+# Otherwise, we create it ourselves.
+version = "0.1.0"
+with open("./python/lsst/alert/packet/version.py", "w") as f:
+    print(f"""
+__all__ = ("__version__", )
+__version__='{version}'""", file=f)
+
 # All configuration is in setup.cfg.
-setup()
+setup(version=f"{version}")
