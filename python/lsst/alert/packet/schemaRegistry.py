3a79f788ffaa1ae777869b8da2644190b850585b
John Parejko <parejkoj@uw.edu>
2023-04-27 15:31:54 -0700

    Add v5 schema, to match RFC-906 APDB schema
    Update examples to new schema.
    Update latest.txt to v5.
    Use v5 as default in schemaRegistry

--- python/lsst/alert/packet/schemaRegistry.py
+++ python/lsst/alert/packet/schemaRegistry.py
@@ -28,6 +28,7 @@ import zlib
 
 __all__ = ["SchemaRegistry"]
 
+
 class SchemaRegistry(object):
     """A registry for alert schemas.
 
@@ -127,7 +128,7 @@ class SchemaRegistry(object):
                                      sort_keys=True).encode('utf-8'))
 
     @classmethod
-    def from_filesystem(cls, root=None, schema_root="lsst.v3_0.alert"):
+    def from_filesystem(cls, root=None, schema_root="lsst.v5_0.alert"):
         """Populate a schema registry based on the filesystem.
 
         Walk the directory tree from the root provided, locating files named
